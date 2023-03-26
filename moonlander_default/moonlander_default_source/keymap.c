16eb66b58dda6710727a6c404b38297f610b104d
natsuyasai <yasai038tomato@gmail.com>
2023-03-26 12:02:02 +0900

    変更

--- moonlander_default/moonlander_default_source/keymap.c
+++ moonlander_default/moonlander_default_source/keymap.c
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTRL,       KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_EQUAL,                                                                       KC_MINUS,       KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_ENTER,       
     KC_LSHIFT,      KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RSHIFT,      
     KC_LCTRL,       KC_LGUI,        KC_LALT,        KC_DELETE,      KC_SPACE,       KC_ESCAPE,                                                                                                      TG(1),          KC_BSPACE,      KC_UP,          KC_LEFT,        KC_DOWN,        KC_RIGHT,       
-    KC_SPACE,       MO(1),          TG(2),                          KC_LBRACKET,    KC_RBRACKET,    KC_QUOTE
+    MO(1),          KC_LSHIFT,      TG(2),                          KC_LBRACKET,    KC_RBRACKET,    KC_QUOTE
   ),
   [1] = LAYOUT_moonlander(
     KC_ESCAPE,      KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_F6,                                          KC_TRANSPARENT, KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,         KC_F12,         
